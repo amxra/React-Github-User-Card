f26f7d1779a044557110821c453b03277decb46e
amxra <amiradediran@gmail.com>
2019-10-01 21:51:35 +0100

    Import axios, create User Component and send api request using axios

---
+++ github-card/src/components/user.js
@@ -0,0 +1,24 @@
+import React from 'react';
+ import axios from 'axios';
+ import UserCard from './UserCard';
+
+  class MainUser extends React.Component {
+
+      componentDidMount() {
+         axios.get(`https://api.github.com/users/amxra`)
+         .then(response => {
+             this.props.setMainUser(response.data)
+         })
+         .catch(err => {
+             console.log(err.message)
+         })
+     }
+
+      render() {
+         return (
+             <UserCard user={this.props.user}/>
+         )
+     }
+ }
+
+  export default MainUser; 
